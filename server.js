3983669b4fc12a36fe9752f6250f70987caf156d
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-08-01 16:21:49 -0400

    Require cors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 PORT = 5001
+
+app.use(cors())
 
 app.listen(PORT, () => {
   console.log(`Server started at ${PORT}`)
