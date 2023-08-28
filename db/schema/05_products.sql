3dacb746fe89f20e8a2f0b2c59633278ad00817f
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-08-28 13:16:05 -0400

    Create schemas

---
+++ db/schema/05_products.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS products CASCADE;
+
+CREATE TABLE products (
+
+  id SERIAL PRIMARY KEY NOT NULL,
+  sku VARCHAR(16) NOT NULL UNIQUE,
+  category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
+  style_id INTEGER REFERENCES styles(id) ON DELETE CASCADE,
+  color_id INTEGER REFERENCES colors(id) ON DELETE CASCADE,
+
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  image1 VARCHAR(512),
+  image2 VARCHAR(512),
+  image3 VARCHAR(512),
+  price INTEGER NOT NULL DEFAULT 0,
+  disp BOOLEAN NOT NULL DEFAULT false
+  
+)
---
+++ db/schema/06_product_sizes.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS product_sizes CASCADE;
+
+CREATE TABLE product_sizes (
+
+  id VARCHAR(16) PRIMARY KEY NOT NULL,
+  product_id INTEGER REFERENCES products(id) ON DELETE CASCADE,
+  size_id INTEGER REFERENCES sizes(id) ON DELETE CASCADE
+
+)
