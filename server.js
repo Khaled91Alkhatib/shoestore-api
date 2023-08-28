fb75b779c03e17a12e02cfc90d88ee7e0a6174b6
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-08-28 13:17:36 -0400

    Require pg and implement new Client database

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-PORT = 5001
+require("dotenv").config();
 
-app.use(cors())
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const PORT = process.env.PORT || 5001;
+
+const { Client } = require('pg');
+const db = new Client({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+db.connect();
+
+app.use(cors());
 
 app.listen(PORT, () => {
-  console.log(`Server started at ${PORT}`)
-})
+  console.log(`Server started at ${PORT}`);
+});
