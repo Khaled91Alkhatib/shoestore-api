881a8ce733f0871c71acbc5e19f5ffbd2acea0b0
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-08-29 10:30:38 -0400

    Set new seeds manually for now until we make it dynamic from app

---
+++ db/seeds/06_carousel.sql
@@ -0,0 +1,10 @@
+INSERT INTO carousel
+  (image1, image2, image3, image4, image5, image6)
+  VALUES (
+    '/Freewheel-Grey-01.jpg',
+    '/Catalyst-Pro-White-02.jpg',
+    '/ColdPack-Ice-Black-03.jpg',
+    '/Icepack-Brown-02.jpg',
+    '/Moab-Adventure-Brown-01.jpg',
+    '/Moab-2-Decon-Mid-Green-02.jpg'
+  );
