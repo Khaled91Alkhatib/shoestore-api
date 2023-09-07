e79b417b362dd18cf90a74f0217b3767aa9ccce2
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-09-07 11:18:10 -0400

    Add products by id route

--- routes/products.js
+++ routes/products.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const { getAllProducts } = require('../db/queries/products/01_getAllProducts');
+const { getProductById } = require('../db/queries/products/06_getProductById');
 
 module.exports = (db) => {
+
   router.get("/", (req, res) => {
     const allProducts = getAllProducts(db);
 
@@ -11,6 +13,21 @@ module.exports = (db) => {
       .then(([data]) => {
         const products = data.rows;
         res.json({ products });
+        return;
+      })
+      .catch(err => {
+        res.status(500).json(`error: ${err.message}`);
+      });
+  });
+
+  router.get("/:id", (req, res) => {
+    const currentId = req.params.id;
+    const productById = getProductById(db, currentId);
+
+    Promise.all([productById])
+      .then(([data]) => {
+        const product = data.rows[0];
+        res.json({ product });
         return;
       })
       .catch(err => {
