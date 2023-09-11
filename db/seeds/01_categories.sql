b3b42802b19f0346b5a161ff27c18ad61e3f8484
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-09-11 15:43:45 -0400

    Change text to lowercase

--- db/seeds/01_categories.sql
+++ db/seeds/01_categories.sql
@@ -1,2 +1,2 @@
-INSERT INTO categories (cat) VALUES ('Men');
-INSERT INTO categories (cat) VALUES ('Women');
+INSERT INTO categories (cat) VALUES ('men');
+INSERT INTO categories (cat) VALUES ('women');
